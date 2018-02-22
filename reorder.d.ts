b700fb7a059a277d6fe626a1e67709ea73cd98c7
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2018-02-22 15:29:10 +0100

    fix typescript decl

--- reorder.d.ts
+++ reorder.d.ts
@@ -1,22 +1,23 @@
-export = reorder
+export = reorder;
 export as namespace reorder;
 
 declare namespace reorder {
     version: string;
     debug: boolean;
-    type Distance = (a: number, b: number) : number;
+    type Distance = (a: number, b: number) => number;
     type Order = number[];
     function cmp_number_asc(a: number, b: number): number;
     function cmp_number_desc(a: number, b: number): number;
     function flatten(a: any[], b: any[]): any[];
+    interface Tree {}
     interface LeafOrder {
-        function order(root: Tree): Order;
-        function distance(dist: Distance): LeafOrder;
-        function distance(): Distance;
-        function linkage(linkage: string): LeafOrder;
-        function linkage(): string;
-        function distance_matrix(matrix: number[][]): LeafOrder;
-        function reorder(matrix: number[][]): Order;
+        order(root: Tree): Order;
+        distance(dist: Distance): LeafOrder;
+        distance(): Distance;
+        linkage(linkage: string): LeafOrder;
+        linkage(): string;
+        distance_matrix(matrix: number[][]): LeafOrder;
+        reorder(matrix: number[][]): Order;
     }
     function optimal_leaf_order() : LeafOrder;
 }
