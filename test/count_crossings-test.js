b2d9ada4d635ee626ee6e51019021ac761190e2d
Curran Kelleher <curran.kelleher@gmail.com>
2021-01-02 12:20:17 -0500

    Manual cleanup of count_crossings-test.js

--- test/count_crossings-test.js
+++ test/count_crossings-test.js
@@ -1,50 +1,50 @@
 const reorder = require('../dist/reorder.cjs');
 
-const vows = require('vows'),
-  assert = require('assert'),
-  seedrandom = require('seedrandom');
-//var    jf = require('jsonfile');
+const vows = require('vows');
+const assert = require('assert');
+const seedrandom = require('seedrandom');
 
 Math.seedrandom('reorder');
 
 const suite = vows.describe('reorder.count_crossings');
 
 function naive_count_crossings(graph, north, south) {
-  let i;
-  let j;
-  let e1;
-  let e2;
-  let v;
-  let count = 0;
   const inv_north = reorder.inverse_permutation(north);
   const inv_south = reorder.inverse_permutation(south);
+
+  let count = 0;
   let links = [];
-  for (i = 0; i < north.length; i++) {
-    v = north[i];
+
+  for (let i = 0; i < north.length; i++) {
+    const v = north[i];
     links = links.concat(
       graph
         .outEdges(v)
         .map((e) => [inv_north[e.target.index], inv_south[e.source.index]])
     );
   }
-  for (i = 0; i < links.length; i++) {
-    e1 = links[i];
-    for (j = i + 1; j < links.length; j++) {
-      e2 = links[j];
-      if ((e1[0] < e2[0] && e1[1] > e2[1]) || (e1[0] > e2[0] && e1[1] < e2[1]))
+  for (let i = 0; i < links.length; i++) {
+    const e1 = links[i];
+    for (let j = i + 1; j < links.length; j++) {
+      const e2 = links[j];
+      if (
+        (e1[0] < e2[0] && e1[1] > e2[1]) ||
+        (e1[0] > e2[0] && e1[1] < e2[1])
+      ) {
         count++;
+      }
     }
   }
   return count;
 }
 
 function dotest(mat) {
-  const graph = reorder.mat2graph(mat, true),
-    comps = graph.components(),
-    comp = comps.reduce((a, b) => (a.length > b.length ? a : b));
+  const graph = reorder.mat2graph(mat, true);
+  const comps = graph.components();
+  const comp = comps.reduce((a, b) => (a.length > b.length ? a : b));
   comp.sort((a, b) => a - b);
-  const layer1 = comp.filter((n) => graph.outEdges(n).length != 0),
-    layer2 = comp.filter((n) => graph.inEdges(n).length != 0);
+  const layer1 = comp.filter((n) => graph.outEdges(n).length != 0);
+  const layer2 = comp.filter((n) => graph.inEdges(n).length != 0);
   //console.time('fast_crossings');
   const c1 = reorder.count_crossings(graph, layer1, layer2);
   //console.timeEnd('fast_crossings');
@@ -64,33 +64,33 @@ suite.addBatch({
   count_crossings: {
     simple() {
       const graph = reorder
-          .graph()
-          .nodes([
-            { id: 0 },
-            { id: 1 },
-            { id: 2 },
-            { id: 3 },
-            { id: 4 },
-            { id: 5 },
-          ])
-          .links([
-            { source: 0, target: 0 },
-            { source: 1, target: 1 },
-            { source: 1, target: 2 },
-            { source: 2, target: 0 },
-            { source: 2, target: 3 },
-            { source: 2, target: 4 },
-            { source: 3, target: 0 },
-            { source: 3, target: 2 },
-            { source: 4, target: 3 },
-            { source: 5, target: 2 },
-            { source: 5, target: 4 },
-          ])
-          .directed(true)
-          .init(),
-        comp = graph.components()[0],
-        layer1 = comp.filter((n) => graph.outEdges(n).length != 0),
-        layer2 = comp.filter((n) => graph.inEdges(n).length != 0);
+        .graph()
+        .nodes([
+          { id: 0 },
+          { id: 1 },
+          { id: 2 },
+          { id: 3 },
+          { id: 4 },
+          { id: 5 },
+        ])
+        .links([
+          { source: 0, target: 0 },
+          { source: 1, target: 1 },
+          { source: 1, target: 2 },
+          { source: 2, target: 0 },
+          { source: 2, target: 3 },
+          { source: 2, target: 4 },
+          { source: 3, target: 0 },
+          { source: 3, target: 2 },
+          { source: 4, target: 3 },
+          { source: 5, target: 2 },
+          { source: 5, target: 4 },
+        ])
+        .directed(true)
+        .init();
+      const comp = graph.components()[0];
+      const layer1 = comp.filter((n) => graph.outEdges(n).length != 0);
+      const layer2 = comp.filter((n) => graph.inEdges(n).length != 0);
 
       assert.equal(naive_count_crossings(graph, layer1, layer2), 12);
       assert.equal(reorder.count_crossings(graph, layer1, layer2), 12);
