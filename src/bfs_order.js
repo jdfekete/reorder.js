ad0a5018bc2976e610660d0b2cdae88828f61cb6
Curran Kelleher <curran.kelleher@gmail.com>
2021-01-02 10:02:34 -0500

    Manual cleanup of bfs_order.js

--- src/bfs_order.js
+++ src/bfs_order.js
@@ -4,15 +4,14 @@ import { bfs } from './bfs';
 export const bfs_order = (graph, comps) => {
   if (!comps) comps = graph.components();
 
-  let i;
-  let comp;
   const order = [];
 
-  for (i = 0; i < comps.length; i++) {
-    comp = comps[i];
+  for (let i = 0; i < comps.length; i++) {
+    const comp = comps[i];
     bfs(graph, comp[0], (v, c) => {
       if (c >= 0 && v != c) order.push(v);
     });
   }
+
   return order;
 };
