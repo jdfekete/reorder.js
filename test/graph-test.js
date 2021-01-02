bd7c87c06c214ad7f1bdbd26764c413781cd87cc
Curran Kelleher <curran.kelleher@gmail.com>
2021-01-02 12:22:35 -0500

    Manual cleanup of graph-test.js

--- test/graph-test.js
+++ test/graph-test.js
@@ -1,18 +1,18 @@
 const reorder = require('../dist/reorder.cjs');
 
-const vows = require('vows'),
-  assert = require('assert');
+const vows = require('vows');
+const assert = require('assert');
 
 const suite = vows.describe('reorder.graph');
 
 suite.addBatch({
   graph: {
     simple() {
-      const nodes = [{ id: 0 }, { id: 1 }, { id: 2 }],
-        links = [
-          { source: 0, target: 1 },
-          { source: 1, target: 2 },
-        ];
+      const nodes = [{ id: 0 }, { id: 1 }, { id: 2 }];
+      const links = [
+        { source: 0, target: 1 },
+        { source: 1, target: 2 },
+      ];
       const graph = reorder.graph(nodes, links).init();
       assert.equal(graph.nodes().length, 3);
       assert.equal(graph.links().length, 2);
