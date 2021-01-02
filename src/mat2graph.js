9b9056c7d6418ef36b55ae72ea065ebc637d1777
Curran Kelleher <curran.kelleher@gmail.com>
2021-01-02 11:40:38 -0500

    Manual cleanup of mat2graph.js

--- src/mat2graph.js
+++ src/mat2graph.js
@@ -5,18 +5,14 @@ export function mat2graph(mat, directed) {
   const nodes = [];
   const links = [];
   let max_value = Number.NEGATIVE_INFINITY;
-  let i;
-  let j;
-  let v;
-  let m;
 
-  for (i = 0; i < n; i++) nodes.push({ id: i });
+  for (let i = 0; i < n; i++) nodes.push({ id: i });
 
-  for (i = 0; i < n; i++) {
-    v = mat[i];
-    m = directed ? 0 : i;
+  for (let i = 0; i < n; i++) {
+    const v = mat[i];
+    const m = directed ? 0 : i;
 
-    for (j = m; j < v.length; j++) {
+    for (let j = m; j < v.length; j++) {
       if (j == nodes.length) nodes.push({ id: j });
       if (v[j] !== 0) {
         if (v[j] > max_value) max_value = v[j];
