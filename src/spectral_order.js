9326bef8f903d20f396b89dca43f96b4eeb55ae0
Curran Kelleher <curran.kelleher@gmail.com>
2021-01-02 12:09:49 -0500

    Manual cleanup of spectral_order.js

--- src/spectral_order.js
+++ src/spectral_order.js
@@ -4,17 +4,13 @@ import { sort_order } from './sort_order';
 import { permute } from './permute';
 
 export function spectral_order(graph, comps) {
-  let i,
-    vec,
-    comp,
-    perm,
-    order = [];
+  let order = [];
   if (!comps) comps = graph.components();
 
-  for (i = 0; i < comps.length; i++) {
-    comp = comps[i];
-    vec = fiedler_vector(laplacian(graph, comp));
-    perm = sort_order(vec);
+  for (let i = 0; i < comps.length; i++) {
+    const comp = comps[i];
+    const vec = fiedler_vector(laplacian(graph, comp));
+    const perm = sort_order(vec);
     order = order.concat(permute(comp, perm));
   }
   return order;
