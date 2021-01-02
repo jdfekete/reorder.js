162db7fd1d20d32ed52a9e9e0f80b09b4ab1def0
Curran Kelleher <curran.kelleher@gmail.com>
2021-01-02 12:23:55 -0500

    Manual cleanup of graph2mat-test.js

--- test/graph2mat-test.js
+++ test/graph2mat-test.js
@@ -1,15 +1,14 @@
 const reorder = require('../dist/reorder.cjs');
 
-const vows = require('vows'),
-  assert = require('assert');
+const vows = require('vows');
+const assert = require('assert');
 const seedrandom = require('seedrandom');
 Math.seedrandom('reorder');
 
 const suite = vows.describe('reorder.graph2mat');
 
 function remove_zeroes(mat) {
-  let i, j;
-  for (i = mat.length - 1; i >= 0; i--) {
+  for (let i = mat.length - 1; i >= 0; i--) {
     const row = mat[i];
     if (row.some((a) => a > 0)) {
       //console.log('remove row %d', i);
@@ -17,7 +16,7 @@ function remove_zeroes(mat) {
     }
     mat.pop();
   }
-  for (j = mat[0].length - 1; j >= 0; j--) {
+  for (let j = mat[0].length - 1; j >= 0; j--) {
     if (mat.some((row) => row[j] != 0)) {
       //console.log('remove column %d', j);
       break;
@@ -43,17 +42,17 @@ suite.addBatch({
       assert.deepEqual(m2, mat);
     },
     lesssimple() {
-      const mat = reorder.random_matrix(0.2, 10),
-        graph = reorder.mat2graph(mat),
-        m2 = reorder.graph2mat(graph);
+      const mat = reorder.random_matrix(0.2, 10);
+      const graph = reorder.mat2graph(mat);
+      const m2 = reorder.graph2mat(graph);
       assert.deepEqual(m2, mat);
     },
     hard() {
       for (let i = 10; i < 100; i += 20) {
         for (let j = 10; j < 100; j += 20) {
-          const mat = remove_zeroes(reorder.random_matrix(0.2, i, j, false)),
-            graph = reorder.mat2graph(mat, true),
-            m2 = reorder.graph2mat(graph);
+          const mat = remove_zeroes(reorder.random_matrix(0.2, i, j, false));
+          const graph = reorder.mat2graph(mat, true);
+          const m2 = reorder.graph2mat(graph);
           assert.deepEqual(m2, mat);
         }
       }
