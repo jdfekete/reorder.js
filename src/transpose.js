905a02d48901cd5592d48e428569ee37b6e6994c
Curran Kelleher <curran.kelleher@gmail.com>
2021-01-02 12:11:05 -0500

    Manual cleanup of transpose.js

--- src/transpose.js
+++ src/transpose.js
@@ -1,12 +1,13 @@
 export function transposeSlice(a, start = 0, end = a[0].length) {
   const m = a.length;
   const n = end;
-  let i = start - 1;
-  let j;
   const b = new Array(end - start);
+
+  let i = start - 1;
+
   while (++i < n) {
     b[i] = new Array(m);
-    j = -1;
+    let j = -1;
     while (++j < m) b[i - start][j] = a[j][i];
   }
   return b;
