c10afb1b05938e9e86096117001615bd0e5c41eb
Curran Kelleher <curran.kelleher@gmail.com>
2021-01-02 12:06:33 -0500

    Manual cleanup of poweriteration_n

--- src/poweriteration.js
+++ src/poweriteration.js
@@ -46,44 +46,37 @@ export function poweriteration_n(v, p, init, eps, start) {
 
   const n = v.length;
   const b = Array(p);
-  let i;
-  let j;
-  let k;
-  let l;
-  let bk;
-  let d;
-  let row;
   let tmp = Array(n);
   let s = 100;
   const eigenvalue = Array(p);
 
   assert(n == v[0].length, 'poweriteration needs a square matrix');
   if (!init) {
-    for (i = 0; i < p; i++) {
-      row = b[i] = random_array(n);
+    for (let i = 0; i < p; i++) {
+      const row = (b[i] = random_array(n));
       eigenvalue[i] = normalize(row);
     }
   } else {
-    for (i = 0; i < p; i++) {
+    for (let i = 0; i < p; i++) {
       b[i] = init[i].slice(); // copy
       eigenvalue[i] = normalize(b[i]);
     }
   }
   if (!start) start = 0;
 
-  for (k = start; k < p; k++) {
-    bk = b[k];
+  for (let k = start; k < p; k++) {
+    let bk = b[k];
     while (s-- > 0) {
       // Orthogonalize vector
-      for (l = 0; l < k; l++) {
-        row = b[l];
-        d = dot(bk, row);
-        for (i = 0; i < n; i++) bk[i] -= d * row[i];
+      for (let l = 0; l < k; l++) {
+        const row = b[l];
+        const d = dot(bk, row);
+        for (let i = 0; i < n; i++) bk[i] -= d * row[i];
       }
 
-      for (i = 0; i < n; i++) {
+      for (let i = 0; i < n; i++) {
         tmp[i] = 0;
-        for (j = 0; j < n; j++) tmp[i] += v[i][j] * bk[j];
+        for (let j = 0; j < n; j++) tmp[i] += v[i][j] * bk[j];
       }
       eigenvalue[k] = normalize(tmp);
       if (dot(tmp, bk) > 1 - eps) break;
