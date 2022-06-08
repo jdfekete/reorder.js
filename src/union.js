4ab7e0ceb5f810221ad974ba0d9ea20f42091c3c
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2022-06-08 08:33:41 +0200

    fix indent

--- src/union.js
+++ src/union.js
@@ -2,7 +2,7 @@
 export function union(matrices) {
   let pile = [];
   for (let i = 0; i < matrices[0].length; i++) {
-    if (! pile[i]) {
+    if (!pile[i]) {
       pile.push([]);
     }
     for (let j = 0; j < matrices[0][0].length; j++) {
